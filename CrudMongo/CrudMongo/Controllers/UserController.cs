fa61d4617938b67661b7704d0ab560ccfebfee74
João Vitor Clavilho <jv.clavilho@hotmail.com>
2025-10-21 20:34:19 -0300

    InitialCommit

---
+++ CrudMongo/CrudMongo/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using CrudMongo.Entities;
+using CrudMongo.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly MongoDbService mongoDbService;
+
+        public UserController(MongoDbService mongoDbService)
+        {
+            this.mongoDbService = mongoDbService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+
+            await mongoDbService.CreateAsync(user);
+            return Ok(user);
+
+        }
+
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            await mongoDbService.DeleteAsync(id);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await mongoDbService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] string id, [FromBody] User user)
+        {
+            await mongoDbService.UpdateAsync(id, user);
+            return Ok();
+        }
+    }
+}
---
+++ CrudMongo/CrudMongo/Service/MongoDbService.cs
@@ -0,0 +1,49 @@
+﻿using CrudMongo.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace CrudMongo.Service;
+
+public class MongoDbService
+{
+    private readonly IMongoCollection<User> _userCollection;
+
+    public MongoDbService(IOptions<MongoDbSettings> mongoDbSettings)
+    {
+        MongoClient client = new MongoClient(mongoDbSettings.Value.ConnectionURI);
+        IMongoDatabase database = client.GetDatabase(mongoDbSettings.Value.DatabaseName);
+        _userCollection = database.GetCollection<User>(mongoDbSettings.Value.CollectionName);
+
+    }
+    public async Task<List<User>> GetAllAsync()
+    {
+        var ussers = await _userCollection.Find(new BsonDocument()).ToListAsync();
+        return ussers;
+    }
+    public async Task CreateAsync(User user)
+    {
+        try
+        {
+            await _userCollection.InsertOneAsync(user);
+            return;
+        }
+        catch (Exception ex)
+        {
+
+            throw;
+        }
+
+    }
+    public async Task DeleteAsync(string id)
+    {
+        FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
+        await _userCollection.DeleteOneAsync(filter);
+    }
+
+    public async Task UpdateAsync(string id, User user)
+    {
+        FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
+        await _userCollection.ReplaceOneAsync(filter, user);
+    }
+}
